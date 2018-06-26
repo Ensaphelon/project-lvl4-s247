b3321d56aad0c306d2ede335376507b4db46d897
Ramil <musaev.ramil@gmail.com>
2018-06-26 11:11:27 +0400

    Channels components implemented as a pure function

--- src/components/Channels.jsx
+++ src/components/Channels.jsx
@@ -1,15 +1,19 @@
 import React from 'react';
 
-export default class Channels extends React.Component{
-  render(){
-    const { channels } = this.props;
-    if (channels.length){
-      return <ul className="list-group">
-        {channels.map((channel, index) => {
-          return <li className='list-group-item list-group-item-action' key={index}>{channel.name}</li>
-        })}
+const Channels = (props) => {
+  const { channels } = props;
+  if (channels.length) {
+    return (
+      <ul className="list-group">
+        {channels.map((channel, index) => (
+          <li className="list-group-item list-group-item-action" key={index}>
+            {channel.name}
+          </li>
+        ))}
       </ul>
-    }
-    return null;
+    );
   }
+  return null;
 };
+
+export default Channels;
