73099c5079f9acbcef520c33410bc4006fa046c3
Ramil <musaev.ramil@gmail.com>
2018-06-30 17:55:45 +0400

    renderChannelItem removed

--- README.md
+++ README.md
@@ -5,3 +5,5 @@
 [![Build Status](https://travis-ci.org/Ensaphelon/project-lvl4-s247.svg?branch=master)](https://travis-ci.org/Ensaphelon/project-lvl4-s247)
 
 [Demo](https://ensaphelon-slack.herokuapp.com/)
+
+Please wait for a minute until the heroku services start
--- src/components/Channels.jsx
+++ src/components/Channels.jsx
@@ -2,10 +2,6 @@ import React from 'react';
 import cn from 'classnames';
 import isArray from 'lodash';
 
-const renderChannelItem = (className, id, name) => (
-  <li className={className} key={id}>{name}</li>
-);
-
 const Channels = ({channels: { list }, currentChannelId}) => {
   if (!isArray(list) && list.length === 0) {
     return null;
@@ -17,7 +13,7 @@ const Channels = ({channels: { list }, currentChannelId}) => {
           'list-group-item list-group-item-action': true,
           active: id === currentChannelId,
         });
-        return renderChannelItem(className, id, name);
+        return <li className={className} key={id}>{name}</li>
       })}
     </ul>
   );
